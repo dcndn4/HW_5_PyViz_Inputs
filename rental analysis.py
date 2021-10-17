4a19d26108a335900476220b0477e0524d8d4b64
dcndn4 <stokes@iphouse.com>
2021-10-17 09:40:48 -0500

    Initial file established

---
+++ rental analysis.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 17 09:31:25 2021
+
+@author: CS_Knit_tinK_SC
+"""
+
+
+# San Francisco Housing Cost Analysis
+
+# In this assignment, you will perform fundamental analysis for the San Francisco housing market
+#  to allow potential real estate investors to choose rental investment properties.
+
+#%%
